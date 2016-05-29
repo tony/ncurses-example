aeb9eba40f671e5391a21b64588a8ea45d0f4c75
Tony Narlock <tony@git-pull.com>
2016-05-29 16:06:15 -0500

    Update clang-format

--- src/util.h
+++ src/util.h
@@ -2,19 +2,23 @@
 // from libjingle, bsd 3-clause
 #ifndef UNUSED
 #define UNUSED(x) Unused(static_cast<const void*>(&x))
-#define UNUSED2(x, y) Unused(static_cast<const void*>(&x)); \
-    Unused(static_cast<const void*>(&y))
-#define UNUSED3(x, y, z) Unused(static_cast<const void*>(&x)); \
-    Unused(static_cast<const void*>(&y)); \
-    Unused(static_cast<const void*>(&z))
-#define UNUSED4(x, y, z, a) Unused(static_cast<const void*>(&x)); \
-    Unused(static_cast<const void*>(&y)); \
-    Unused(static_cast<const void*>(&z)); \
-    Unused(static_cast<const void*>(&a))
-#define UNUSED5(x, y, z, a, b) Unused(static_cast<const void*>(&x)); \
-    Unused(static_cast<const void*>(&y)); \
-    Unused(static_cast<const void*>(&z)); \
-    Unused(static_cast<const void*>(&a)); \
-    Unused(static_cast<const void*>(&b))
+#define UNUSED2(x, y)                   \
+  Unused(static_cast<const void*>(&x)); \
+  Unused(static_cast<const void*>(&y))
+#define UNUSED3(x, y, z)                \
+  Unused(static_cast<const void*>(&x)); \
+  Unused(static_cast<const void*>(&y)); \
+  Unused(static_cast<const void*>(&z))
+#define UNUSED4(x, y, z, a)             \
+  Unused(static_cast<const void*>(&x)); \
+  Unused(static_cast<const void*>(&y)); \
+  Unused(static_cast<const void*>(&z)); \
+  Unused(static_cast<const void*>(&a))
+#define UNUSED5(x, y, z, a, b)          \
+  Unused(static_cast<const void*>(&x)); \
+  Unused(static_cast<const void*>(&y)); \
+  Unused(static_cast<const void*>(&z)); \
+  Unused(static_cast<const void*>(&a)); \
+  Unused(static_cast<const void*>(&b))
 inline void Unused(const void*) {}
 #endif  // UNUSED
